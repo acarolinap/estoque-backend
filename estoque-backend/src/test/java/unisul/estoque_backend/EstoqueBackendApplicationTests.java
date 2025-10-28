d7caa3350eff641f0f98027f399deb6ecab11230
MonnoCatto <MonnoCatto@users.noreply.github.com>
2025-10-28 17:32:30 -0300

    First commit

---
+++ estoque-backend/src/test/java/unisul/estoque_backend/EstoqueBackendApplicationTests.java
@@ -0,0 +1,12 @@
+package unisul.estoque_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
